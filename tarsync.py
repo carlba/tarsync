0b9a81612077798e339c3072c9352c293b70c687
Carl Bäckström <genzorg@gmail.com>
2013-12-25 20:47:04 +0100

    It is now possible to create symlinks.
    -The cli argument is called --symlink and is used in the same way as
    compress and decompress but a symlink to the program path is created.
    
    Issue #14

--- tarsync.py
+++ tarsync.py
@@ -10,6 +10,7 @@ import stat
 
 if "windows" in platform.system().lower():
     import tarfile
+    import win32file
 else:
     from tarfile_progress import tarfile_progress as tarfile
 
@@ -143,21 +144,25 @@ class ProgramHandler(object):
                 if self.os in path.keys():
                     logging.debug("test")
                     expanded_path = os.path.expandvars(path[self.os])
-                    (spath,sfile) = os.path.split(expanded_path)
-                    os.chdir(self.out_path)
-                    self.old_dir = os.getcwd()
-                    if not os.path.exists(spath):
-                        os.makedirs(spath)
-                    os.chdir(spath)
-                    compressed_fname = "%s_%s.tar.gz" % (program["name"],path["name"])
-                    self.handle_compression(compressed_fname,sfile)
-                    os.chdir(self.old_dir)
+                    self.handle_program_path(program, path, expanded_path)
+
         else:
             logging.debug("The program %s is not in the filter." % repr(program["name"]))
 
     def handle_programs(self,programs):
         for program in programs:
         	self.handle_program(program)
+
+    def handle_program_path(self,program,path,program_path):
+        (spath,sfile) = os.path.split(program_path)
+        os.chdir(self.out_path)
+        self.old_dir = os.getcwd()
+        if not os.path.exists(spath):
+            os.makedirs(spath)
+        os.chdir(spath)
+        compressed_fname = "%s_%s.tar.gz" % (program["name"],path["name"])
+        self.handle_compression(compressed_fname,sfile)
+        os.chdir(self.old_dir)
 
     def do_work(self):
         if self.os == "cygwin":
@@ -166,6 +171,8 @@ class ProgramHandler(object):
             self.out_path = cygpath.get_cygwin_path()
         else:
             self.out_path = os.path.expandvars(self.config["path"][self.os])
+
+        self.symlink_path = os.path.expandvars(self.config["symlink_path"][self.os])
 
         logging.debug("The outpath for tarsync is %s" % self.out_path)
 
@@ -208,6 +215,19 @@ class ProgramLister(ProgramHandler):
         print program["name"]
 
 
+class ProgramSymlinker(ProgramHandler):
+    """docstring for ClassName"""
+
+    def handle_program_path(self,program,path,program_path):
+        (spath,sfile) = os.path.split(program_path)
+
+        if self.os == "windows":
+            os.path.expandvars(self.config["path"][self.os])
+            win32file.CreateSymbolicLink(program_path, "%s/%s" %(self.symlink_path,sfile), 1)
+
+
+
+
 
 def main():
     #programs = construct_dict()
@@ -223,12 +243,13 @@ def main():
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-c", "--compress", nargs="*")
     group.add_argument("-d", "--decompress", nargs="*")
+    group.add_argument("-s", "--symlink", nargs="*")
     group.add_argument("-l", "--list", action="store_true")
 
     #logging.debug(repr(locals()))
     args = parser.parse_args()
 
-    if not (args.compress or args.decompress or args.list):
+    if not (args.compress or args.decompress or args.list or args.symlink):
         parser.error('No action requested, add --compress or --decompress')
 
     if args.compress:
@@ -237,6 +258,9 @@ def main():
         ph.do_work()
     if args.decompress:
         ph = ProgramDecompresser(config, filter=args.decompress)
+        ph.do_work()
+    if args.symlink:
+        ph = ProgramSymlinker(config, filter=args.symlink)
         ph.do_work()
     if args.list:
         ph = ProgramLister(config)
