c5ac3f46ee33d250851433e523c599adfb0382d7
Carl Bäckström <genzorg@gmail.com>
2014-12-20 13:11:44 +0100

    - Made the source more objectoriented - Improved the config-handler

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 setup(entry_points={'console_scripts': ['tarsync = tarsync.tarsync:main']},
       name="tarsync",
       version='1.0.0',
-      options = {},
+      options={},
       description='Syncs application data between systems',
       author='Carl Backstrom',
       packages=find_packages(),
-      data_files=[('/etc/tarsync', ['config/config.json'])]
+      data_files=[('/etc/tarsync', ['config/config.json'])],
+      install_requires=["appdirs"]
 )
--- tarsync/tarsync.py
+++ tarsync/tarsync.py
@@ -6,6 +6,10 @@ import argparse
 import shutil
 import stat
 import appdirs
+import pprint
+
+
+pp = pprint.PrettyPrinter(indent=4)
 
 
 if "windows" in platform.system().lower():
@@ -20,6 +24,10 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+class MissingPathError(Exception):
+    pass
 
 
 #import logging
@@ -42,8 +50,30 @@ def progressprint(complete, path=False):
 class ConfigHandler(object):
 
     def __init__(self, config_file):
-        self._config_file = config_file
+        #import ipdb;ipdb.set_trace()
+        self._config_file = os.path.basename(config_file)
         self._config = {}
+
+    def __locate_config_directory(self, config_file):
+        paths = []
+
+        user_config_path = appdirs.user_config_dir("tarsync")
+        paths.append(user_config_path)
+
+        site_config_path = appdirs.site_config_dir("tarsync")
+        paths.append(site_config_path)
+
+        for path in paths:
+            if os.path.exists(path):
+                config_path = path
+                break
+        else:
+            config_path = os.path.join(
+                os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "config")
+        logger.info("Looking for config in %s" % paths)
+        logger.info("The configuration is read from %s" % config_path)
+
+        return config_path
 
     def __store_dict_to_file(self, filename, thedict):
         with open(filename, "w+") as jsonfile:
@@ -55,9 +85,15 @@ class ConfigHandler(object):
 
     def load(self, config_file=None):
         if config_file:
-            self._config = self.__read_dict_from_file(config_file)
+            config_file = config_file
         else:
-            self._config = self.__read_dict_from_file(self._config_file)
+            config_file = self._config_file
+
+        located_config_directory = self.__locate_config_directory(config_file)
+        located_config_file = os.path.join(
+            located_config_directory, config_file)
+
+        self._config = self.__read_dict_from_file(located_config_file)
 
     @property
     def config(self):
@@ -108,85 +144,73 @@ def safe_remove_folder(folder):
         shutil.rmtree(folder)
 
 
-class ProgramHandler(object):
+class Path(object):
 
-    """Does something"""
+    def __init__(self, os_paths, name=None):
+        self._os_paths = os_paths
+        self._os = self.__get_os()
+        self._name = name
 
-    def __init__(self, config_handler, filter=None):
-        super(ProgramHandler, self).__init__()
-        self.os = self.get_os()
-        self._config = config_handler.config
-        self.filter = filter
-        logger.debug("The filter contains:%s" % repr(self.filter))
-
-    def get_os(self):
+    def __get_os(self):
         if "cygwin" in platform.system().lower():
             ret = "cygwin"
         else:
             ret = platform.system().lower()
         return ret
 
-    def handle_compression(self, compressed_fname, sfile):
-        pass
-
-    def handle_programs(self, programs):
-        for program in programs:
-            self.handle_program(program)
-
-    def handle_program(self, program):
-        if program["name"] in self.filter:
-            for path in program["paths"]:
-                logger.debug("The path for %s is %s" % (program["name"], path))
-                logger.debug("self.os is %s" % (self.os))
-
-                if self.os == "cygwin":
-                    win_path = path["windows"]
-                    cygpath = CygwinPath(win_path)
-                    path["cygwin"] = cygpath.get_cygwin_path()
-
-                if self.os in path.keys():
-                    logger.debug("test")
-                    expanded_path = os.path.expandvars(path[self.os])
-                    self.handle_program_path(program, path, expanded_path)
-
+    @property
+    def path(self):
+        try:
+            path = self._os_paths[self._os]
+        except KeyError as e:
+            if self._os == "cygwin":
+                win_path = self._os_paths["windows"]
+                cygpath = CygwinPath(win_path)
+                path = cygpath.get_cygwin_path()
+            else:
+                raise MissingPathError(
+                    "The path for %s is missing in the config-file" % self._os)
         else:
-            logger.debug("The program %s is not in the filter." %
-                         repr(program["name"]))
+            expanded_path = os.path.expandvars(path)
 
-    def handle_program_path(self, program, path, program_path):
-        (spath, sfile) = os.path.split(program_path)
-        os.chdir(self.out_path)
+        return expanded_path
+
+    def make(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+    def __generate_name(self, program_name):
+        #import ipdb;ipdb.set_trace()
+        compressed_fname = "%s_%s.tar.gz" % (program_name, self._name)
+        return compressed_fname
+
+    def __prepare(self, out_path):
+        (spath, sfile) = os.path.split(self.path)
+        os.chdir(out_path.path)
         self.old_dir = os.getcwd()
-        if not os.path.exists(spath):
-            os.makedirs(spath)
+        self.make()
         os.chdir(spath)
-        compressed_fname = "%s_%s.tar.gz" % (program["name"], path["name"])
-        self.handle_compression(compressed_fname, sfile)
-        os.chdir(self.old_dir)
 
-    def do_work(self):
-        if self.os == "cygwin":
-            win_path = os.path.expandvars(self._config["path"]["windows"])
-            cygpath = CygwinPath(win_path)
-            self.out_path = cygpath.get_cygwin_path()
+        return spath, sfile
+
+    def compress(self, program_name, out_path):
+        spath, sfile = self.__prepare(out_path)
+        compressed_fname = self.__generate_name(program_name)
+        tar = tarfile.open(compressed_fname, "w:gz")
+        if "windows" in self._os:
+            tar.add(sfile, arcname=compressed_fname)
         else:
-            self.out_path = os.path.expandvars(self._config["path"][self.os])
-        self.symlink_path = os.path.expandvars(
-            self._config["symlink_path"][self.os])
-        logger.debug("The outpath for tarsync is %s" % self.out_path)
-        self.programs = self._config["programs"]
-        self.handle_programs(self.programs)
+            tar.add(sfile, arcname=compressed_fname, progress=progressprint)
+        tar.close()
+        shutil.move(
+            compressed_fname, os.path.join(self.old_dir, compressed_fname))
 
-
-class ProgramDecompresser(ProgramHandler):
-
-    """docstring for ClassName"""
-
-    def handle_compression(self, compressed_fname, sfile):
+    def decompress(self, program_name, out_path):
+        spath, sfile = self.__prepare(out_path)
+        compressed_fname = self.__generate_name(program_name)
         tar = tarfile.open(
-            os.path.join(self.out_path, compressed_fname), "r:gz")
-        # logger.debug(tar.getmembers())
-        if "windows" in self.os:
+            os.path.join(out_path.path, compressed_fname), "r:gz")
+        if "windows" in self._os:
             tar.extractall()
         else:
             tar.extractall(progress=progressprint)
@@ -196,46 +220,34 @@ class ProgramDecompresser(ProgramHandler):
         shutil.move(compressed_fname, sfile)
 
 
-class ProgramCompresser(ProgramHandler):
+class Program(object):
 
-    """docstring for ClassName"""
+    """docstring for Program"""
 
-    def handle_compression(self, compressed_fname, sfile):
-        tar = tarfile.open(compressed_fname, "w:gz")
-        if "windows" in self.os:
-            tar.add(sfile, arcname=compressed_fname)
-        else:
-            tar.add(sfile, arcname=compressed_fname, progress=progressprint)
-        tar.close()
-        shutil.move(
-            compressed_fname, os.path.join(self.old_dir, compressed_fname))
+    def __init__(self, **kwargs):
+        super(Program, self).__init__()
+        self.name = kwargs.pop("name")
+        self.paths = kwargs.pop("paths")
+        self.out_path = kwargs.pop("out_path")
 
+    def install(self):
+        for path_entry in self.paths:
+            path = Path(os_paths=path_entry)
+            out_path = Path(os_paths=self.out_path)
+            out_path.make()
 
-class ProgramLister(ProgramHandler):
+    def compress(self):
+        for path_entry in self.paths:
+            out_path = Path(os_paths=self.out_path)
+            out_path.make()
+            path = Path(os_paths=path_entry, name=path_entry["name"])
+            path.compress(self.name, out_path)
 
-    """A class that lists all programs in a config-file"""
-
-    def handle_program(self, program):
-        print program["name"]
-
-
-class ProgramSymlinker(ProgramHandler):
-
-    """A class that symlinks programs specified in the config-file"""
-
-    def handle_program_path(self, program, path, program_path):
-        (spath, sfile) = os.path.split(program_path)
-
-        if os.path.exists(program_path):
-            if os.path.islink(program_path):
-                if "windows" in platform.system().lower():
-                    os.rmdir(program_path)
-                else:
-                    os.remove(program_path)
-            else:
-                os.rename(program_path, program_path + ".bak")
-
-        os.symlink("%s/%s" % (self.symlink_path, sfile), program_path)
+    def decompress(self):
+        for path_entry in self.paths:
+            out_path = Path(os_paths=self.out_path)
+            path = Path(os_paths=path_entry, name=path_entry["name"])
+            path.decompress(self.name, out_path)
 
 
 def main():
@@ -248,31 +260,10 @@ def main():
     logger.debug("Starting logger")
     logger.info("The application is named %s" % app_name)
 
-    paths = []
-
-    user_config_path = appdirs.user_data_dir("tarsync")
-    paths.append(user_config_path)
-
-    site_config_path = appdirs.site_data_dir("tarsync")
-    if "xdg" in site_config_path:
-        site_config_path = site_config_path.replace("/xdg", "")
-    paths.append(site_config_path)
-
-    found = False
-    for path in paths:
-        if os.path.exists(path):
-            config_path = path
-            break
-    else:
-        config_path = os.path.join(
-            os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "config")
-
-    logger.info("The configuration is read from %s" % config_path)
-
-    logger.debug("APPNAME: %s" % app_name)
-
-    config = ConfigHandler(os.path.join(config_path, app_name + ".json"))
+    config = ConfigHandler(app_name + ".json")
     config.load()
+
+    logger.debug(json.dumps(config.config, indent=4))
 
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
@@ -280,27 +271,53 @@ def main():
     group.add_argument("-d", "--decompress", nargs="*")
     group.add_argument("-s", "--symlink", nargs="*")
     group.add_argument("-l", "--list", action="store_true")
+    group.add_argument("-i", "--install", nargs="*")
 
     # logger.debug(repr(locals()))
     args = parser.parse_args()
 
-    if not (args.compress or args.decompress or args.list or args.symlink):
+    if not (args.compress or args.decompress or args.list or args.symlink or args.install):
         parser.error('No action requested, add --compress or --decompress')
 
-    if args.compress:
-        logger.debug(args.compress)
-        ph = ProgramCompresser(config, filter=args.compress)
-        ph.do_work()
-    if args.decompress:
-        ph = ProgramDecompresser(config, filter=args.decompress)
-        ph.do_work()
-    if args.symlink:
-        ph = ProgramSymlinker(config, filter=args.symlink)
-        ph.do_work()
-    if args.list:
-        ph = ProgramLister(config)
-        ph.do_work()
+    config_programs = config.config["programs"]
+    dict_programs = {
+        program["name"]: program for (index, program) in enumerate(config_programs)}
 
+    def get_programs(config, filter=None):
+        programs = []
+
+        for program_name in dict_programs:
+            if filter and not program_name in filter:
+                continue
+            program = Program(name=program_name,
+                              paths=dict_programs[program_name]["paths"],
+                              out_path=config.config["path"])
+            programs.append(program)
+        return programs
+
+    args_list = ["compress", "decompress", "symlink", "list", "install"]
+
+    for arg_name in args_list:
+        arg = getattr(args, arg_name)
+        if arg:
+            programs = get_programs(config, arg)
+            for program in programs:
+                method = getattr(program, arg_name)
+                method()
+
+
+    # for program in programs:
+    #     if args.compress:
+    #         program.compress()
+    #     if args.decompress:
+    #         program.decompress()
+    #     if args.symlink:
+    #         ph = ProgramSymlinker(config, filter=args.symlink)
+    #         ph.do_work()
+    #     if args.list:
+    #         print program.name
+    #     if args.install:
+    #         program.install()
 if __name__ == '__main__':
     main()
     pass
